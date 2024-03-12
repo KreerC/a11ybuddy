9651f8cc200c24b3782a6ee5a48282e5d0213da0
Casey Kreer <15277434+KreerC@users.noreply.github.com>
2024-03-12 14:45:59 +0100

    Remove expansive license notice on each file for a more accessible DX

--- src/a11yBuddy/Router.php
+++ src/a11yBuddy/Router.php
@@ -1,25 +1,30 @@
 <?php
 
-/*
-   Copyright 2024 Casey Kreer
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 namespace A11yBuddy;
 
 class Router
 {
+
+    private static string $requestUri = "/";
+    private static string $requestMethod = "GET";
+
+    /**
+     * @return string The current request URI.
+     */
+    public static function getRequestUri(): string
+    {
+        return self::$requestUri;
+    }
+
+    /**
+     * @return string The current request method.
+     */
+    public static function getRequestMethod(): string
+    {
+        return self::$requestMethod;
+    }
+
+
     private $routes = [
         "GET" => [],
         "POST" => [],
@@ -27,9 +32,10 @@ class Router
         "DELETE" => []
     ];
 
-    private static string $requestUri = "/";
-    private static string $requestMethod = "GET";
-
+    /**
+     * Upon instantiation, the Router will set the request URI and method.
+     * These are useful constants for the rest of the application.
+     */
     public function __construct()
     {
         $uri = $_SERVER["REQUEST_URI"] ?? "/";
@@ -111,22 +117,6 @@ class Router
         $pattern = preg_replace('/{([a-zA-Z0-9_]+)}/', '(?P<$1>[a-zA-Z0-9_]+)', $pattern);
         $pattern = '/^' . $pattern . '$/';
         return $pattern;
-    }
-
-    /**
-     * @return string The current request URI.
-     */
-    public static function getRequestUri(): string
-    {
-        return self::$requestUri;
-    }
-
-    /**
-     * @return string The current request method.
-     */
-    public static function getRequestMethod(): string
-    {
-        return self::$requestMethod;
     }
 
 }
